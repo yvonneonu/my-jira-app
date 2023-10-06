a6e68842d569cdc0e75b1776c589787152fa63f7
unknown <yvonneonuorah@gmail.com>
2023-10-06 17:39:01 +0100

    jira

---
+++ app/components/BackLogItems.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import Image from "next/image";
+import { MoreCircle } from "iconsax-react";
+import { Droppable, Draggable } from "react-beautiful-dnd";
+
+export default function BackLogItems({ column, tasks }) {
+  return (
+    <div className="w-[280px] bg-white h-[450px] rounded p-4">
+      <div>
+        <div className="flex flex-row justify-between p-[10px]">
+          <div className="flex flex-row items-center gap-[10px]">
+            <MoreCircle size="15" variant="TwoTone" color="#F1F4F4" />
+            <div className="text-sky-950 text-base font-medium tracking-tight">
+              {column.title}
+            </div>
+          </div>
+
+          <div className="w-6 h-6  bg-blue-600/25 rounded items-center flex justify-center">
+            <div className="text-center text-blue-800 text-[13px] font-normal ">
+              {column.taskNum}
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-[0px] border border-slate-50"></div>
+      </div>
+      <Droppable droppableId={column.id}>
+        {(droppableProvided, droppableSnapshot) => (
+          <div
+            ref={droppableProvided.innerRef}
+            {...droppableProvided.droppableProps}
+          >
+            {tasks.map((task, index) => (
+              <Draggable
+                key={task?.id}
+                draggableId={`${task.id}`}
+                index={index}
+              >
+                {(draggableProvided, draggableSnapshot) => {
+                  const outlineColor = draggableSnapshot.isDragging
+                    ? "#2D313E"
+                    : "transparent";
+
+                  const boxShadow = draggableSnapshot.isDragging
+                    ? "0 5px 10px rgba(0, 0, 0, 0.6)"
+                    : "unset";
+
+                  return (
+                    <div
+                      className="flex w-full justify-center mt-5"
+                      style={{
+                        outline: `2px solid ${outlineColor} ${boxShadow}`,
+                      }}
+                      ref={draggableProvided.innerRef}
+                      {...draggableProvided.draggableProps}
+                      {...draggableProvided.dragHandleProps}
+                    >
+                      <div className="w-[90%] gap-[16px] flex flex-col bg-white rounded border border-sky-950 border-opacity-5 p-[10px]">
+                        <div className="w-[218px] text-slate-600 text-[13px] font-medium">
+                          {task?.description}
+                        </div>
+
+                        <div className="flex flex-row items-center justify-between">
+                          <div className="flex flex-row gap-[5px]">
+                            <div className="w-4 h-4 rounded">
+                              <Image
+                                className="w-full h-full"
+                                width={92}
+                                height={64}
+                                src={task?.icon1}
+                              />
+                            </div>
+
+                            <div className="text-slate-400 text-[13px] font-light leading-normal">
+                              {task?.taskNumber}
+                            </div>
+                          </div>
+                          <div className="flex gap-[10px] items-center">
+                            <div className="w-4 h-4 rounded">
+                              <Image
+                                className="w-full h-full"
+                                width={92}
+                                height={64}
+                                src={task?.icon2}
+                              />
+                            </div>
+                            <div className="w-6 h-6 rounded border opacity-10-border-sky-950 flex items-center justify-center">
+                              <div className="text-center text-sky-950 text-[13px] font-normal">
+                                {task?.taskStatus}
+                              </div>
+                            </div>
+
+                            <div className="w-6 h-6 rounded-full">
+                              <Image
+                                className="w-full h-full"
+                                width={92}
+                                height={64}
+                                src={task?.icon3}
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                }}
+              </Draggable>
+            ))}
+          </div>
+        )}
+      </Droppable>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,113 +1,372 @@
-import Image from 'next/image'
+"use client";
+
+import {
+  AddSquare,
+  ArrangeHorizontal,
+  ArrowLeft,
+  Calendar,
+  Discover,
+  Filter,
+  Home2,
+  Messenger,
+  MoreCircle,
+  People,
+  SearchNormal1,
+  Setting5,
+  TaskSquare,
+  Timer,
+} from "iconsax-react";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import React from "react";
+import { DragDropContext } from "react-beautiful-dnd";
+
+const BackLogItems = dynamic(() => import("./components/BackLogItems"), {
+  ssr: false,
+});
+
+function SidebarItem({ text, icon, index, activeIndex, onClick }) {
+  const isActive = index === activeIndex;
+  const backgroundStyle = isActive
+    ? "bg-blue-600 p-[15px] rounded text-white"
+    : "p-[15px]";
+
+  return (
+    <div
+      className={`flex flex-row justify-between items-center w-[240px] cursor-pointer ${backgroundStyle}`}
+      onClick={() => onClick(index)}
+    >
+      <div className="flex flex-row gap-4">
+        {icon}
+        <div className="text-[13px] font-normal tracking-tight">{text}</div>
+      </div>
+
+      {isActive && <MoreCircle size="15" variant="TwoTone" />}
+    </div>
+  );
+}
+
+const reorderColumnList = (sourceCol, startIndex, endIndex) => {
+  const newTaskIds = Array.from(sourceCol.taskIds);
+
+  const [removed] = newTaskIds.splice(startIndex, 1);
+  newTaskIds.splice(endIndex, 0, removed);
+  const newColumn = {
+    ...sourceCol,
+    taskIds: newTaskIds,
+  };
+
+  return newColumn;
+};
 
 export default function Home() {
+  const [activeIndex, setActiveIndex] = React.useState(null);
+
+  const handleItemClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const [state, setState] = React.useState(initialData);
+
+  const onDragEnd = (result) => {
+    console.log("Drag end:", result);
+
+    const { destination, source } = result;
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const sourceCol = state.columns[source.droppableId];
+    const destinationCol = state.columns[destination.droppableId];
+
+    if (sourceCol.id === destinationCol.id) {
+      const newColumn = reorderColumnList(
+        sourceCol,
+        source.index,
+        destination.index
+      );
+      const newState = {
+        ...state,
+        columns: {
+          ...state.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+      setState(newState);
+      return;
+    }
+
+    const startTaskIds = Array.from(sourceCol.taskIds);
+    const [removed] = startTaskIds.splice(source.index, 1);
+    const newStartCol = {
+      ...sourceCol,
+      taskIds: startTaskIds,
+    };
+
+    const endTaskIds = Array.from(destinationCol.taskIds);
+    endTaskIds.splice(destination.index, 0, removed);
+    const newEndCol = {
+      ...destinationCol,
+      taskIds: endTaskIds,
+    };
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newStartCol.id]: newStartCol,
+        [newEndCol.id]: newEndCol,
+      },
+    };
+    setState(newState);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <main className="inter relative overflow-hidden">
+        <div className="w-full h-[100vh]">
+          <nav className=" w-full h-[10vh] bg-white">
+            <div className=" flex flex-row items-center bg-white  border-b-2 border-slate-50">
+              <div className="w-[20vw] justify-center flex p-[20px] border-r-2">
+                <Image
+                  className="w-full h-full "
+                  width={92}
+                  height={64}
+                  src="/hero.svg"
+                />
+              </div>
+
+              <div className="flex flex-row justify-between w-full  bg-white p-[15px] ml-3">
+                <div className="flex flex-row gap-[20px] items-center relative p-[5px] ml-5">
+                  <input
+                    className="w-[250px] h-8 bg-slate-400 bg-opacity-5 rounded p-[25px] pl-[50px]"
+                    placeholder="Search"
+                  />
+                  <div className="absolute top-[20px] left-[15px]">
+                    <SearchNormal1 size="18" color="#8C97AC" />
+                  </div>
+                  <div className="w-8 h-8 bg-slate-400 bg-opacity-5 rounded flex items-center justify-center ">
+                    <Setting5 size="25" color="#8C97AC" />
+                  </div>
+                  <div className="w-8 h-8 bg-slate-400 bg-opacity-5 rounded flex items-center justify-center ">
+                    <Filter size="25" color="#8C97AC" />
+                  </div>
+                </div>
+                <div className="p-[5px] mr-5">
+                  <AddSquare size="32" color="#2D70FD" />{" "}
+                </div>
+              </div>
+            </div>
+          </nav>
+
+          <div className="flex flex-row  p-[20px] ">
+            <div className="  h-full border-slate-50  bg-white w-[19.8vw] items- justify-center flex ">
+              <div className="flex justify-center p-[20px]">
+                <div className="flex flex-col gap-[25px] justify-center items-center">
+                  <SidebarItem
+                    text="Dashboard"
+                    icon={<Home2 size="20" />}
+                    index={0}
+                    activeIndex={activeIndex}
+                    onClick={handleItemClick}
+                  />
+                  <SidebarItem
+                    text="Feedback"
+                    icon={<Messenger size="20" />}
+                    index={1}
+                    activeIndex={activeIndex}
+                    onClick={handleItemClick}
+                  />
+                  <SidebarItem
+                    text="Task"
+                    icon={<TaskSquare size="20" />}
+                    index={2}
+                    activeIndex={activeIndex}
+                    onClick={handleItemClick}
+                  />
+                  <SidebarItem
+                    text="Roadmap"
+                    icon={<Discover size="20" />}
+                    index={3}
+                    activeIndex={activeIndex}
+                    onClick={handleItemClick}
+                  />
+                  <SidebarItem
+                    text="Feedback"
+                    icon={<ArrangeHorizontal size="20" />}
+                    index={4}
+                    activeIndex={activeIndex}
+                    onClick={handleItemClick}
+                  />
+                </div>
+
+                <div className="flex flex-grow flex-col absolute bottom-0 items-start justify-start gap-[15px] w-[19vw] p-[15px] ">
+                  <div className="flex flex-row gap-5 items-center">
+                    <div className="w-6 h-6 bg-amber-300 rounded-md flex items-center justify-center ">
+                      <div className="text-center text-white text-sm font-medium leading-none tracking-tight">
+                        E
+                      </div>
+                    </div>
+                    <div className="text-sky-950 text-[13px] font-normal tracking-tight">
+                      Neil Larkins
+                    </div>
+                  </div>
+                  <div className="flex flex-row gap-5 items-center">
+                    <div className="w-6 h-6 bg-amber-300 rounded-md flex items-center justify-center ">
+                      <div className="text-center text-white text-sm font-medium leading-none tracking-tight">
+                        E
+                      </div>
+                    </div>
+                    <div className="text-sky-950 text-[13px] font-normal tracking-tight">
+                      Epodpay Inc.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex h-[10vh] justify-between items-center w-full">
+              <div className="  flex flex-row items-center">
+                <ArrowLeft size="32" color="#405175" />
+
+                <div className="ml-[25px] flex flex-row gap-[20px] items-center">
+                  <div className="flex flex-row gap-2">
+                    <Timer size="15" color="#405175" />
+                    <div className="text-slate-600 text-[13px] font-light  leading-normal">
+                      SPR-06
+                    </div>
+                  </div>
+
+                  <div className="flex flex-row gap-2 ">
+                    <People size="15" color="#2D70FD" />
+                    <div className="w-[58px] text-slate-600 text-[13px] font-light  leading-normal">
+                      TEAM-10
+                    </div>
+                  </div>
+                  <div className="text-sky-950 text-[13px] font-medium ">
+                    New Sprint Name
+                  </div>
+
+                  <Calendar size="15" color="#FF8A65" />
+
+                  <People size="15" color="#405175" />
+
+                  <div className="w-[129px] flex items-center justify-center h-8 bg-blue-200 rounded">
+                    <div className="text-blue-600 text-[13px] text-center font-medium  tracking-tight">
+                      Complete Sprint
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mr-[20px] flex flex-row gap-[10px] items-center">
+                <div className="w-6 h-6  bg-slate-100 rounded">
+                  <TaskSquare size="20" color="#8C97AC" />
+                </div>
+
+                <Image
+                  className="w-full h-full "
+                  width={92}
+                  height={64}
+                  src="/Group.svg"
+                />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div className="">
+          <div className=" h-[450px]  absolute top-[135px] p-[20px] right-[80px]  mt-3   w-[900px]">
+            <div className="w-[1000px]  bg-[#F1F4F4] p-[25px] flex  flex-col gap-[25px]">
+              <div className=" justify-center flex items-center gap-[20px] ">
+                {state.columnOrder.map((columnId) => {
+                  const column = state.columns[columnId];
+                  const tasks = column.taskIds.map(
+                    (taskId) => state.task[taskId]
+                  );
+                  return (
+                    <BackLogItems
+                      key={column.id}
+                      column={column}
+                      tasks={tasks}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </DragDropContext>
+  );
 }
+
+const initialData = {
+  task: {
+    1: {
+      id: 1,
+      description: "Configure Next.js Application",
+      taskNumber: "TSK-01",
+      taskStatus: "8",
+      icon1: "/Icons.svg",
+      icon2: "/chart.svg",
+      icon3: "/Ellipse.svg",
+    },
+    2: {
+      id: 2,
+      description: "Create SideBar Naviagtion Menu",
+      taskNumber: "EPC-02",
+      taskStatus: "7",
+      icon1: "/Icons.svg",
+      icon2: "/chart.svg",
+      icon3: "/Ellipse.svg",
+    },
+    3: {
+      id: 3,
+      description: "Create Page Layout",
+      taskNumber: "EPC-11",
+      taskStatus: "13",
+      icon1: "/Icons.svg",
+      icon2: "/chart.svg",
+      icon3: "/Ellipse.svg",
+    },
+    // 4: {
+    //   id: 4,
+    //   description: "Create Page Footer",
+    //   taskNumber: "TSK-010",
+    //   taskStatus: "5",
+    //   icon1: "/Icons.svg",
+    //   icon2: "/chart.svg",
+    //   icon3: "/Ellipse.svg",
+    // },
+  },
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "BACK-LOG",
+      taskIds: [1, 2, 3],
+      taskNum: "72",
+    },
+    "column-2": {
+      id: "column-2",
+      title: "TO-DO",
+      taskIds: [],
+      taskNum: "22",
+    },
+    "column-3": {
+      id: "column-3",
+      title: "IN-PROGRESS",
+      taskIds: [],
+      taskNum: "32",
+    },
+    // "column-4": {
+    //   id: "column-4",
+    //   title: "DONE",
+    //   taskIds: [],
+    //   taskNum: "12",
+    // },
+  },
+
+  columnOrder: ["column-1", "column-2", "column-3"],
+};
